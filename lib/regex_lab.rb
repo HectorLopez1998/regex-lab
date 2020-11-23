bda582a459677d5750bfb738b9045695628935cf
HectorLopez1998 <hectorlopezp1998@gmail.com>
2020-11-23 11:35:27 -0500

    commit tree b3c587908a60653f10b30d5d20bc68c051d278c6

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,24 @@
 def starts_with_a_vowel?(word)
-
+if word.start_with?("a","e", "i", "o", "u", "A", "E", "I", "O", "U")
+    return true
+else 
+    return false
+end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    # array = []
+    # for text in text 
+    # if text.start_with?("un") && text.end_with?("ing")
+    #     array << text
+    # end
+    # return array
+    # end
+    
 end
 
 def words_five_letters_long(text)
-
+text.grep(/^\w{5}\s/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
